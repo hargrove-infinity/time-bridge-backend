760fb281e7eae3d61b65b1a4e8b14bcb42e8f8a8
hargrove <hargrove.infinity@gmail.com>
2025-10-03 18:36:57 +0300

    added rest negative tests

--- src/test/validateMiddleware.test.ts
+++ src/test/validateMiddleware.test.ts
@@ -45,4 +45,55 @@ describe("validate middleware", () => {
 
     expect(next).not.toHaveBeenCalled();
   });
+
+  test("should fail: email is invalid, password is valid", async () => {
+    const request = httpMocks.createRequest({
+      body: { email: "abc", password: "password" },
+    });
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+
+    const middleware = validate({ schema: userValidationSchema });
+    middleware(request, response, next);
+
+    expect(response.statusCode).toBe(400);
+
+    const data = response._getData();
+
+    expect(data.errors).toEqual([ERROR_MESSAGES.EMAIL_INVALID]);
+  });
+
+  test("should fail: email is valid, password is undefined", async () => {
+    const request = httpMocks.createRequest({
+      body: { email: "mail@mail.com", password: undefined },
+    });
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+
+    const middleware = validate({ schema: userValidationSchema });
+    middleware(request, response, next);
+
+    expect(response.statusCode).toBe(400);
+
+    const data = response._getData();
+
+    expect(data.errors).toEqual([ERROR_MESSAGES.PASSWORD_UNDEFINED]);
+  });
+
+  test("should fail: email is valid, password is invalid", async () => {
+    const request = httpMocks.createRequest({
+      body: { email: "mail@mail.com", password: "pass" },
+    });
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+
+    const middleware = validate({ schema: userValidationSchema });
+    middleware(request, response, next);
+
+    expect(response.statusCode).toBe(400);
+
+    const data = response._getData();
+
+    expect(data.errors).toEqual([ERROR_MESSAGES.PASSWORD_LENGTH]);
+  });
 });
