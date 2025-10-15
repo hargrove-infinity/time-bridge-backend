50cdcb8bd8cbdc6a0bf256331ca8282ce4e8f4d6
hargrove <hargrove.infinity@gmail.com>
2025-10-15 16:12:11 +0300

    applied expectToEqualJwtPayload into verifyCreateUserRequest

--- src/test/utils/verifyCreateUserRequest.ts
+++ src/test/utils/verifyCreateUserRequest.ts
@@ -1,9 +1,10 @@
 import { Express } from "express";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 import request from "supertest";
 import { paths } from "../../constants";
 import { TEST_USER_EMAIL, TEST_USER_PASSWORD } from "../constants";
+import { expectToEqualJwtPayload } from "./expectToEqualJwtPayload";
 
 export async function verifyCreateUserRequest(app: Express): Promise<void> {
   const response = await request(app).post(paths.users.base).send({
@@ -11,23 +12,11 @@ export async function verifyCreateUserRequest(app: Express): Promise<void> {
     password: TEST_USER_PASSWORD,
   });
 
-  expect(response.body).toStrictEqual({
-    payload: expect.any(String),
-  });
+  expect(response.body).toStrictEqual({ payload: expect.any(String) });
 
-  const decoded = jwt.decode(response.body.payload!);
+  const decoded = jwt.decode(response.body.payload);
 
-  expect(typeof decoded).not.toBe("string");
-  expect(typeof decoded).toBe("object");
+  expectToEqualJwtPayload(decoded);
 
-  expect(mongoose.Types.ObjectId.isValid((decoded as JwtPayload)!._id)).toBe(
-    true
-  );
-
-  expect(decoded).toStrictEqual({
-    _id: expect.any(String),
-    email: TEST_USER_EMAIL,
-    iat: expect.any(Number),
-    exp: expect.any(Number),
-  });
+  expect(mongoose.Types.ObjectId.isValid(decoded._id)).toBe(true);
 }
