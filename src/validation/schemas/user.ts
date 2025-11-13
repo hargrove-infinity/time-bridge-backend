76da736233129f9d129fca521eb56f39aeab6fb2
hargrove <hargrove.infinity@gmail.com>
2025-11-13 16:26:03 +0200

    updated userValidationSchema

--- src/validation/schemas/user.ts
+++ src/validation/schemas/user.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { ERROR_DEFINITIONS } from "../../constants";
 
-export const userObject = z.object({
+export const userValidationSchema = z.object({
   email: z
     .string(ERROR_DEFINITIONS.EMAIL_UNDEFINED.code)
     .min(1, ERROR_DEFINITIONS.EMAIL_EMPTY.code)
@@ -11,11 +11,4 @@ export const userObject = z.object({
     .min(6, ERROR_DEFINITIONS.PASSWORD_LENGTH.code),
 });
 
-export type CreateUserInput = z.infer<typeof userObject>;
-
-export const userValidationSchema = z
-  .any()
-  .refine((val) => val !== undefined && val !== null, {
-    message: ERROR_DEFINITIONS.AUTH_BODY_UNDEFINED.code,
-  })
-  .pipe(userObject);
+export type CreateUserInput = z.infer<typeof userValidationSchema>;
