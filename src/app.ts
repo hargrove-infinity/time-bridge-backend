619a9e54997829c973f7b22c718cfe3436e43c98
David Y. Stephenson <david@davidystephenson.com>
2025-11-14 12:40:56 -0500

    Add error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import { userRouter } from "./routes";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.use(userRouter);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof SyntaxError && 'body' in err) {
+        console.error(err);
+        return res.status(400).send({ status: 400, message: err.message })
+    }
+    
+    console.error(err)
+    res.status(500).send({ status: 500, message: "Internal Server Error" })
+    next()
+})
 
 export { app };
