0ede5a87d415a044145db1f2a3d9fcf7c520975a
hargrove <hargrove.infinity@gmail.com>
2025-12-05 23:03:39 +0200

    added tests of emailConfirm in userRoutes.test

--- src/test/utils/user/index.ts
+++ src/test/utils/user/index.ts
@@ -1,6 +1,7 @@
 export * from "./expectUserRepoCreateSuccess";
 export * from "./expectUserRepoFindOneSuccess";
 export * from "./expectUserRequestRegisterSuccess";
+export * from "./expectUserRouteEmailConfirmSuccess";
 export * from "./expectUserRouteLoginSuccess";
 export * from "./expectUserRouteRegisterSuccess";
 export * from "./expectUserServiceLoginSuccess";
