01114fc49c8d811f395f61e16ca82905e0a97a47
hargrove <hargrove.infinity@gmail.com>
2025-12-10 19:39:52 +0200

    returned sendEmail to the initial state

--- src/services/email/emailService.ts
+++ src/services/email/emailService.ts
@@ -7,33 +7,24 @@ async function sendEmail(args: SendEmailArgs): SendEmailResult {
   const { transporter, toEmail, subject, html } = args;
 
   try {
-    return [
-      {
-        data: { id: "abcd" },
-        error: null,
-        headers: {},
-      },
-      null,
-    ];
+    const result = await transporter.emails.send({
+      from: envVariables.sendEmailFrom,
+      to: toEmail,
+      subject,
+      html,
+    });
 
-    // const result = await transporter.emails.send({
-    //   from: envVariables.sendEmailFrom,
-    //   to: toEmail,
-    //   subject,
-    //   html,
-    // });
+    if (result.error) {
+      return [
+        null,
+        new ApplicationError({
+          errorDefinition: ERROR_DEFINITIONS.SEND_EMAIL_FAILED,
+          statusCode: 500,
+        }),
+      ];
+    }
 
-    // if (result.error) {
-    //   return [
-    //     null,
-    //     new ApplicationError({
-    //       errorDefinition: ERROR_DEFINITIONS.SEND_EMAIL_FAILED,
-    //       statusCode: 500,
-    //     }),
-    //   ];
-    // }
-
-    // return [result, null];
+    return [result, null];
   } catch (error) {
     return [
       null,
